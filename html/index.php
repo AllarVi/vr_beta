8735ccd6f2305aac5079d08f7fce78c6c3b3f4fa
Erko Teor <erkoteor@hotmail.com>
2015-12-05 19:37:22 +0200

    Html folder setup

---
+++ html/index.php
@@ -0,0 +1,24 @@
+<html>
+<head xmlns:fb="http://www.facebook.com/2008/fbml">
+    <title>Title</title>
+    <meta charset="UTF-8">
+    <script src="js/facebookLogin.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+<!--
+  Below we include the Login Button social plugin. This button uses
+  the JavaScript SDK to present a graphical Login button that triggers
+  the FB.login() function when clicked.
+-->
+<div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false">
+
+</div>
+
+<div id="status">
+</div>
+
+</body>
+</html>
