f7d3772e8ff64d2770875abda00fdbbfa658f8f3
aglo35 <allar.viinamae@hotmail.com>
2015-12-08 19:52:15 +0200

    Testin some PHP session stuff

--- html/Controller.php
+++ html/Controller.php
@@ -3,7 +3,7 @@ include "id_card_utils.php";
 require "config.php";
 
 use Parse\ParseException;
-use Parse\ParseUser;
+use Parse\ParseObject;
 
 /**
  * Created by PhpStorm.
@@ -34,34 +34,75 @@ class Controller
         $ePerson = getEPerson();
 
         if (!$ePerson) {
-//             TODO: echo("Authentication failed.");
+//             TODO: Better handling?
             $this->user->string = "ID kaardiga audentimine ebaõnnestus!";
         } else {
             $this->user->lastName = $ePerson[0];
             $this->user->firstName = $ePerson[1];
             $this->user->nationalID = $ePerson[2];
             $this->user->string = "ID kaardiga audentimine õnnestus edukalt!";
-            $this->signUp($this->user, $this->user->lastName, $this->user->firstName, $this->user->nationalID);
+            $this->saveUserData($this->user, $this->user->lastName, $this->user->firstName, $this->user->nationalID);
+
+            // Starts user session
+            $this->begin_session();
+//            $this->signUp($this->user, $this->user->lastName, $this->user->firstName, $this->user->nationalID);
         }
     }
 
-    private function signUp($user, $lastName, $firstName, $nationalID)
+    private function saveUserData($user, $lastName, $firstName, $nationalID)
     {
-        $parseUser = new ParseUser();
-        $parseUser->set("username", $nationalID);
-        // TODO: password generation
-        $parseUser->set("password", "default");
-        $parseUser->set("firstName", $firstName);
-        $parseUser->set("lastName", $lastName);
+        $userData = new ParseObject($firstName);
+
+        $userData->set("lastName", $lastName);
+        $userData->set("firstName", $firstName);
+        $userData->set("nationalID", $nationalID);
 
         try {
-            $parseUser->signUp();
-            // Hooray! Let them use the app now.
-            $user->parseMessage = "Hooray! User saved into Parse database.";
+            $userData->save();
+            $user->parseMessage = 'New object created with objectId: ' . $userData->getObjectId();
         } catch (ParseException $ex) {
-            // Show the error message somewhere and let the user try again.
-            $user->parseMessage = "Error: " . $ex->getCode() . " " . $ex->getMessage();
+            // Execute any logic that should take place if the save fails.
+            // error is a ParseException object with an error code and message.
+            $user->parseMessage = 'Failed to create new object, with error message: ' . $ex->getMessage();
         }
     }
+
+    private function begin_session()
+    {
+        session_start();
+        session_regenerate_id();
+        $_SESSION['valid'] = 1;
+        $this->debug_to_console(md5($_SERVER['HTTP_USER_AGENT']));
+        $_SESSION['fingerprint'] = md5($_SERVER['HTTP_USER_AGENT']);
+    }
+
+    function debug_to_console($data)
+    {
+
+        if (is_array($data))
+            $output = "<script>console.log( 'Debug Objects: " . implode(',', $data) . "' );</script>";
+        else
+            $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
+
+        echo $output;
+    }
+
+//    private function signUp($user, $lastName, $firstName, $nationalID)
+//    {
+//        $parseUser = new ParseUser();
+//        $parseUser->set("username", $nationalID);
+//        $parseUser->set("password", "default");
+//        $parseUser->set("firstName", $firstName);
+//        $parseUser->set("lastName", $lastName);
+//
+//        try {
+//            $parseUser->signUp();
+//            // Hooray! Let them use the app now.
+//            $user->parseMessage = "Hooray! User saved into Parse database.";
+//        } catch (ParseException $ex) {
+//            // Show the error message somewhere and let the user try again.
+//            $user->parseMessage = "Error: " . $ex->getCode() . " " . $ex->getMessage();
+//        }
+//    }
 
 }
