4f51ba1b90f50d9db50a50b3c3125366b23c98c4
aglo35 <allar.viinamae@hotmail.com>
2015-12-16 00:36:23 +0200

    Minor refactoring from req.php to api.php

--- html/js/common.js
+++ html/js/common.js
@@ -8,7 +8,7 @@ var Search = {
 
             var input = searchInput.val();
 
-            $.getJSON('https://52.29.136.34/req.php?search=' + input, function (json) {
+            $.getJSON('https://52.29.136.34/api.php?search=' + input, function (json) {
                 var searchResult = $('#searchResult');
                 searchResult.text("");
                 if (json.result != 'No result') {
